63e2667068b64fed8dda71ec8d2d26708e62add3
kenzwms <mooter2000@gmail.com>
2016-07-09 11:47:56 -0400

    Add files via upload

--- july_9.php
+++ july_9.php
@@ -1,6 +1,29 @@
-      <form action= "poole_savings.php" method="post">
+<html>
+	
+	<head>
+		<title> July 9th </title>
+	</head>
+	
+
+
+	<form action= "poole_savings.php" method="post">
 		
 		First Name:<input type="text" name="Fname"></br>
 		Last Name:<input type="text" name="Lname"></br>
 		Username:<input type="text" name="Username"></br>
 		Password:<input type="text" name="Password"></br>
+			<input type="submit" name="submit">
+
+<?php
+		require ('dbconnect.php');
+	 
+	 $Fname=$_POST ['Fname'];
+	 $Lname=$_POST ['Lname'];
+	 $Username=$_POST ['Username'];
+	 $Password=$_POST ['Password'];
+	 
+		
+	?>
+					
+</body>
+</html>
