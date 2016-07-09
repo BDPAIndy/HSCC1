f7639518e4e80c94fac8b3c0b093b9c10498e8b2
kenzwms <mooter2000@gmail.com>
2016-07-09 11:13:13 -0400

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbHost= 'localhost';
+$dbUser= 'root';
+$dbPass= '';
+$dbName= 'hscc2016'; 
+
+$con= mysqli_connect($dbHost,$dbUser,$dbPass,$dbName)OR 'DIE DATABASE CONNECTION NOT SUCCESSFUL';
+
+
+?>
