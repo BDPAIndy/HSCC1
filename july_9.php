c9eede15882482515eef59c0d53b6ddb68ba0216
kenzwms <mooter2000@gmail.com>
2016-07-09 11:10:20 -0400

    Add files via upload

---
+++ july_9.php
@@ -0,0 +1,6 @@
+      <form action= "poole_savings.php" method="post">
+		
+		First Name:<input type="text" name="Fname"></br>
+		Last Name:<input type="text" name="Lname"></br>
+		Username:<input type="text" name="Username"></br>
+		Password:<input type="text" name="Password"></br>
